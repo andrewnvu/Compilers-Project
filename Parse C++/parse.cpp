2a2cf46172bfe0ea402b6d41b46688abf33bdcbe
Juan Carrera <juanmiindset@gmail.com>
2018-05-07 21:27:46 -0700

    added C++ parser
    works but unsure about some of the spacing

---
+++ Parse C++/parse.cpp
@@ -0,0 +1,127 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<sstream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+
+	string filename = "file.txt";
+	fstream new_file;
+	fstream input_file;
+	string line;
+
+	input_file.open(filename);
+
+	if (!input_file.is_open())
+	{
+		cout << "Error opening file!";
+		return 1;
+
+	}
+
+	while (getline(input_file, line, '\0'))
+	{
+		while (line.find("(*") != std::string::npos)
+		{
+			size_t Beg = line.find("(*");
+			line.erase(Beg, (line.find("*)", Beg) - Beg) + 2);
+
+		}
+
+		line.erase(std::remove(line.begin(), line.end(), ' '), line.end());
+		line.erase(std::remove(line.begin(), line.end(), '\n'), line.end());
+		line.erase(std::remove(line.begin(), line.end(), '\t'), line.end());
+	}
+
+
+
+	for (int i = 0; i < line.length(); i++)
+	{
+
+		if (line[i] == ';')
+		{
+			line.insert(i + 1, "\n");
+		}
+
+		if (line[i] == ',')
+		{
+			line.insert(i + 1, " ");
+		}
+
+		if (line[i] == '=')
+		{
+			line.insert(i + 1, " ");
+		}
+		if (line[i] == '=')
+		{
+			line.insert(i++, " ");
+		}
+
+		if (line[i] == ':')
+		{
+			line.insert(i + 1, " ");
+		}
+
+		if (line[i] == ':')
+		{
+			line.insert(i++, " ");
+		}
+
+		if (line[i] == '+')
+		{
+			line.insert(i + 1, " ");
+		}
+
+		if (line[i] == '+')
+		{
+			line.insert(i++, " ");
+		}
+
+		if (line[i] == '(')
+		{
+			line.insert(i + 1, " ");
+		}
+
+		if (line[i] == ')')
+		{
+			line.insert(i++, " ");
+		}
+
+	}
+	string var = "VAR";
+	string begg = "BEGIN";
+	string progg = "PROGRAM";
+
+	size_t poss = line.find(var);
+	size_t poss2 = line.find(begg);
+	size_t poss3 = line.find(progg);
+
+	line.insert(poss + 3, "\n");
+	line.insert(poss2 + 6, "\n");
+	line.insert(poss3 + 7, " ");
+
+	cout << line << endl;
+
+	input_file.close();
+
+
+	new_file.open("new_file.txt");
+	if (!new_file.is_open())
+	{
+		cout << "Error opening file!";
+		return 1;
+
+	}
+
+	new_file << line;
+
+	new_file.close();
+
+
+	system("PAUSE");
+	return 0;
+}
